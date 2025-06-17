501afb873b1d29d98ec8c3bd19169c973b1c429f
The Beebz <nyvivianv@gmail.com>
2025-06-17 12:30:50 -0400

    Update animclip.bat
    Added explanation

--- animclip.bat
+++ animclip.bat
@@ -1,2 +1,4 @@
-ffmpeg -i "%~1" -ss 00:09:38 -to 00:10:12 -c:v copy -loglevel error "clip_%~n1.mp4"
+ffmpeg -i "%~1" -ss 00:01:00 -to 00:02:00 -c:v copy -loglevel error "clip_%~n1.mp4"
 pause
+REM Drag and Drop your video onto the batch script to create a clip. This example would create a 1 minute clip 
+REM from between 1:00 to 2:00 in the initial file. 
