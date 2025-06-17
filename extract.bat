02e7f9934e69463e37861e7bb686d83b826b040d
The Beebz <nyvivianv@gmail.com>
2025-06-17 12:59:55 -0400

    Update extract.bat
    Added explanation

--- extract.bat
+++ extract.bat
@@ -1,4 +1,10 @@
-cd World Stars/
+cd Extracted Screenshots/
 mkdir "%~n1"
 cd "%~n1/"
 ffmpeg -i "%~1" -r 0.4 "%~n1-%%04d.png"
+pause
+REM This command extracts screenshots from the input video. 
+REM The first 3 lines make it so that it navigates to the folder "Extracted Screenshots" 
+REM makes a folder with the input name, and then put the extracted files inside
+REM -r measures the rate at which screenshots are captureed
+REM Bigger number means more screenshots. Smaller number lowers the rate of screengrabs taken
