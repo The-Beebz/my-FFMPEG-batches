87cad6aa345b745a4480bf41382121ecf0e7654c
The Beebz <nyvivianv@gmail.com>
2025-06-17 13:45:09 -0400

    Update recordcompress.bat
    Explained script

--- recordcompress.bat
+++ recordcompress.bat
@@ -1,2 +1,4 @@
 ffmpeg -i "%~1" -crf 30 -c:v libx264 -loglevel error  -preset slow "smol_%~n1.mp4"
 pause
+REM Compresses video. higher number for crf means more lossy conversion. 
+REM This one is acceptable for my VODS but your mileage may vary
