a511d49161d1114257679fdbcfcea05384b9b377
The Beebz <nyvivianv@gmail.com>
2025-06-17 13:50:40 -0400

    Update smaller.bat
    Explained and Edited

--- smaller.bat
+++ smaller.bat
@@ -1,1 +1,4 @@
-ffmpeg -i input.mp4 -c:v libx264 -preset veryslow -vf "scale=1280:-1" -c:a copy output.mp4
+ffmpeg -i "%~1" -c:v libx264 -preset slow -vf "scale=960:-1" -c:a copy "downsize_%~n1.mp4".mp4
+pause
+REM makes a video have smaller dimensions
+REM -1 on either width:height means aspect ratio is kept
